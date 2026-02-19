db30e8b4f6825e8c26ef4cb53e00bcdb90f3c0d9
Bhoomika Vankar <bpvankar496@gmail.com>
2026-02-19 13:08:35 +0530

    first commit

--- db.js
+++ db.js
@@ -1,26 +1,3 @@
-// const mongoose = require('mongoose');
-
-// const MONGO_URI = 'mongodb+srv://bpvankar496_db_user:bhoomi@cluster0.dqgw0k5.mongodb.net/healthcare?retryWrites=true&w=majority';
-
-// mongoose.connect(MONGO_URI)
-//   .then(() => console.log('✅ MongoDB Connected'))
-//   .catch(err => console.log('❌ MongoDB Error:', err));
-
-// const patientSchema = new mongoose.Schema({
-//   name: String,
-//   age: Number,
-//   phone: String,
-//   problem: String,
-//   doctor: String,
-//   date: String,
-//   time: String,
-//   createdAt: { type: Date, default: Date.now }
-// });
-
-// const Patient = mongoose.model('Patient', patientSchema);
-// module.exports = { Patient };
-
-
 require('dotenv').config();
 const mongoose = require('mongoose');
 
